2439c38608ddb9c8651ed69a193c806744382910
adilkhan <aomertayevich@gmail.com>
2022-05-03 18:36:55 +0600

    Collections

--- src/com/company/MyHash.java
+++ src/com/company/MyHash.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class MyHash <T extends Comparable<T>> {
     int size = 0;
-    class DataItem {
+    class DataItem implements Comparable<DataItem> {
         private T data;
         private int key;
 
@@ -26,11 +26,28 @@ public class MyHash <T extends Comparable<T>> {
         public void setKey(int key) {
             this.key = key;
         }
+
+        @Override
+        public int compareTo(DataItem o) {
+            return 0;
+        }
     }
 
-    public int hashCode(){
-        return 0;
+    public int hashCode(int key){
+        return key % size;
     }
 
+    MyArrayList<DataItem> hash = new MyArrayList<>();
 
+    public DataItem get(int key) {
+        int index = hashCode(key);
+        while(hash.get(index) != null) {
+            if(hash.get(index).getKey() == key) {
+                return hash.get(index);
+            }
+            index++;
+            index = index % size;
+        }
+        return null;
+    }
 }
