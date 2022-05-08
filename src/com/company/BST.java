8dee3276b454d845395723214d24a935d48c8a4a
adilkhan <aomertayevich@gmail.com>
2022-05-09 02:21:02 +0600

    Collections

--- src/com/company/BST.java
+++ src/com/company/BST.java
@@ -1,13 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class BST<K extends Comparable<K>, V>{
-    private Node root;
     private int size = 0;
+    ArrayList<Node> bst;
 
     private class Node {
         private K key;
         private V value;
-        private Node left, right;
 
         public Node(K key, V value) {
             this.key = key;
@@ -22,14 +23,6 @@ public class BST<K extends Comparable<K>, V>{
             return value;
         }
 
-        public Node getLeft() {
-            return left;
-        }
-
-        public Node getRight() {
-            return right;
-        }
-
         public void setKey(K key) {
             this.key = key;
         }
@@ -37,14 +30,10 @@ public class BST<K extends Comparable<K>, V>{
         public void setValue(V value) {
             this.value = value;
         }
+    }
 
-        public void setLeft(Node left) {
-            this.left = left;
-        }
-
-        public void setRight(Node right) {
-            this.right = right;
-        }
+    public BST(){
+        this.bst = new ArrayList<>();
     }
 
     private int hash(K key) {
@@ -59,16 +48,18 @@ public class BST<K extends Comparable<K>, V>{
     public void put(K key, V value) {
         Node newNode = new Node(key, value);
         if(size == 0) {
-            root = newNode;
-            root.setLeft(null);
-            root.setRight(null);
-        } else if(size == 1) {
-            if ((int)newNode.getKey() > (int)root.getKey()) {
-                root.setRight(newNode);
-            } else {
-                root.setLeft(newNode);
+            bst.add(newNode);
+        } else {
+            int i = 2;
+            while(i < size) {
+                if (hash(key) > size/i) {
+
+                } else {
+
+                }
+                i *= 2;
             }
-        } else {} IIIDDDDDDKKKK omg
+        }
     }
 
     public V get(K key) {}
